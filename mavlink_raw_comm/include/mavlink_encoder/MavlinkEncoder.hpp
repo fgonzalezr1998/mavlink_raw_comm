3e94c4fbb56f41c4dcfe9d16f38eafe773e0a120
fgonzalezr1998 <fergonzaramos@yahoo.es>
2022-04-04 23:19:23 +0200

    Comments added

--- mavlink_raw_comm/include/mavlink_encoder/MavlinkEncoder.hpp
+++ mavlink_raw_comm/include/mavlink_encoder/MavlinkEncoder.hpp
@@ -1,3 +1,38 @@
+
+/*********************************************************************
+*  Software License Agreement (BSD License)
+*
+*   Copyright (c) 2022
+*   All rights reserved.
+*
+*   Redistribution and use in source and binary forms, with or without
+*   modification, are permitted provided that the following conditions
+*   are met:
+*    * Redistributions of source code must retain the above copyright
+*      notice, this list of conditions and the following disclaimer.
+*    * Redistributions in binary form must reproduce the above
+*      copyright notice, this list of conditions and the following
+*      disclaimer in the documentation and/or other materials provided
+*      with the distribution.
+*    * Neither the name of the author nor the names of its
+*      contributors may be used to endorse or promote products derived
+*      from this software without specific prior written permission.
+*   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*   COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*   POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Fernando Gonzalez fergonzaramos@yahoo.es  */
+
 #include <string>
 #include "mavlink_encoder/Digest.hpp"
 #include "status_text/StatusTextEncoder.hpp"
@@ -21,16 +56,44 @@ class MavlinkEncoder
 public:
   MavlinkEncoder(int sys_id, int comp_id);
 
-  // Returns DigestType with full statustext message
-
+  /* Returns a digest with full statustext message encoded following mavlink
+	 * packages specification
+	 *
+	 * @param msg Message to be sended (50 chars maximum)
+	 * @param severity Severity of the statustext message
+	 * 
+	 * @return DigestType that contains the full message encoded and ready
+	 * to be sended to a Pixhawk/Autopilot
+	 * 
+	 * @throws mavlink_encoder::StatusTextEncodeException Thrown
+	 * if the message is larger than 50 bytes
+	 */
   mavlink_encoder::DigestType statusTextMsg(const std::string & msg, 
     const status_text::StatusSeverity & severity);
 
+	/*
+	 * Increase in one the sequence number. It will be encoded in the next package
+	 * 	bytes calculated
+	 */
 	void increaseSeqN();
+
+	/*
+	 * Set specific sequence number. It will be encoded in the next package
+	 * 	bytes calculated
+	 * 
+	 * @param seq sequence numbr to set
+	 */
 	void setSeqN(int seq);
 
 private:
 
+	/*
+	 * Returns the checksum of a given mavlink package
+	 * @param digest digest with the full mavlink package
+	 * 	data (header + payload)
+	 * @param crc_extra additional byte to be used as extra seed for
+	 * 	the crc calculation. It depends on the message type
+	 */
   uint16_t checksum(const mavlink_encoder::DigestType & digest,
 		uint8_t crc_extra);
 
--- mavlink_raw_comm/include/status_text/StatusTextEncoder.hpp
+++ mavlink_raw_comm/include/status_text/StatusTextEncoder.hpp
@@ -1,3 +1,37 @@
+/*********************************************************************
+*  Software License Agreement (BSD License)
+*
+*   Copyright (c) 2022
+*   All rights reserved.
+*
+*   Redistribution and use in source and binary forms, with or without
+*   modification, are permitted provided that the following conditions
+*   are met:
+*    * Redistributions of source code must retain the above copyright
+*      notice, this list of conditions and the following disclaimer.
+*    * Redistributions in binary form must reproduce the above
+*      copyright notice, this list of conditions and the following
+*      disclaimer in the documentation and/or other materials provided
+*      with the distribution.
+*    * Neither the name of the author nor the names of its
+*      contributors may be used to endorse or promote products derived
+*      from this software without specific prior written permission.
+*   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*   COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*   POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Fernando Gonzalez fergonzaramos@yahoo.es  */
+
 #include <string>
 #include "mavlink_encoder/Digest.hpp"
 
@@ -31,26 +65,42 @@ public:
 
   static StatusTextEncoder* getInstance();
 
-  // Store on 'out_digest' DigestType with the statustext Payload
-	// Returns true if everything was ok. Else, returns false
-
+	/*
+	 * Store on 'out_digest' DigestType with the statustext Payload
+	 *
+	 * @param msg Message to be encoded
+	 * @param severity Statustext severity
+	 * @param out_digest DigestType where the calculated payload will be stored
+	 */
   bool composePayload(const std::string & msg, 
     const StatusSeverity & severity,
     mavlink_encoder::DigestType * out_digest);
 
-  // Store on 'out_digest' DigestType with the statustext Header
-
+	/*
+	 * Store on 'out_digest' the DigestType with the statustext Header
+	 *
+	 * @param len_payload Lenght of the message's payload
+	 * @param seq_n Sequence number
+	 * @param sys_id System ID (Given by flight controller)
+	 * @param comp_od Component ID (Given by flight controller)
+	 * @param out_digest DigestType where the calculated header will be stored
+	 */
   void composeHeader(int len_payload, int seq_n,
     int sys_id, int comp_id, mavlink_encoder::DigestType * out_digest);
 
   // Not should be assignable
-
   void operator=(const StatusTextEncoder &) = delete;
 
 protected:
   StatusTextEncoder(){};
 
 private:
+	/*
+	 * Converts std string to char array (without null termination)
+	 *
+	 * @param text Input string
+	 * @param buff Output char array
+	 */
   void string2Buffer(const std::string & text, char * buff);
 };
 
